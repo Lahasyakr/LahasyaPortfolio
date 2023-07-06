bd3d268e132ae231d9e296c457b8ad5027c8b0d9
lahasya <lahasya.lnu001@umb.edu>
2023-07-06 11:40:53 -0400

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ const About = () => {
                         </p>
                     </div>
                     <div className="contact-info">
-                        <a href="resume.pdf" className="resume-link" targer="_blank" download >RESUME</a>
+                        <a href="images/Lahasya_resume.pdf" className="resume-link" targer="_blank" download >RESUME</a>
                         <p>
                             <FaEnvelope className="icon" />
                             <strong><a style={{ textDecoration: "none", color: "black" }} href="mailto:lahasyakr.07@gmail.com" target="blank">lahasyakr.07@gmail.com</a></strong>
