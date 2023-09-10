85f9ff689ff5fd0591917e8ffe968e8f123905d4
lahasya <lahasya.lnu001@umb.edu>
2023-09-10 00:26:33 -0400

    changes in details

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,7 @@ const projects = [
     {
         id: 1,
         title: 'Collaborative Segmentation Software',
+        date: 'Jan 2023 - May 2023',
         image: 'annotate-sync.gif',
         description: 'In Collaboration with Massachusetts General Hospital developed a browser-based online annotation tool for 3D medical image volumes that allow annotations to be shared between users.',
         link: 'https://github.com/oopsuwu/Collaborative-Segmentation-Software/blob/main/README.md',
@@ -12,13 +13,15 @@ const projects = [
     {
         id: 2,
         title: 'CryptoLib',
+        date: 'Jan 2023 - May 2023',
         image: "cryptolib.png",
         description: 'In collaboration with Atsign Company developed a Library for - RSA, AES, Encryption and Decryption, and Base64 encode and decode using Javascript.',
         link: 'https://github.com/Lahasyakr/CS682-Project4b/blob/main/README.md',
     },
     {
-        id:3,
-        title:"Shared DrawingApp",
+        id: 3,
+        title: "Shared DrawingApp",
+        date: 'Dec 2022 - Feb 2023',
         image: "sharedDrawing.png",
         description: 'Real Time Shared Drawing Application using React + (express + socket.io)',
         link: 'https://github.com/Lahasyakr/Shared_DrawingApp',
@@ -36,6 +39,7 @@ const Projects = () => {
                         <div key={index} className="project">
                             <div className="project-details">
                                 <h3>{project.title}</h3>
+                                <div style={{ marginRight: "0px", fontSize: "small", color:"grey" }}>{project.date}</div>
                             </div>
                             <img src={project.image} alt={project.title} />
                             <div className="project-details">
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -23,7 +23,7 @@ const workExperience = [
         location: 'Bangalore, India',
         position: 'Senior Software Engineer',
         duration: 'Oct 2019 - March 2022',
-        description: 'During my tenure at Marlabs Innovations Pvt. Ltd from October 2019 to March 2020, I worked on several projects as a UI Developer. Here are the details of the projects and my roles:',
+        description: 'As a Senior Software Engineer at Marlabs Innovations Pvt. Ltd from October 2019 to March 2022, I played a pivotal role in the success of projects for our esteemed client, the Merck Group. My responsibilities encompassed a wide range of key areas, including independently researching and prototyping project components, crafting responsive web interfaces using React, Redux, and TypeScript, maintaining exceptional code quality with over 90% test coverage and rigorous testing, and contributing to team growth through upskilling sessions, particularly focusing on React hooks. Moreover, I exhibited strong leadership qualities by leading project planning, problem-solving, and decision-making processes, which significantly contributed to the seamless development and timely delivery of projects.:',
         logo: MdWorkHistory, // Replace with the actual logo file name
         projects: [
             {
@@ -36,7 +36,7 @@ const workExperience = [
                 projectName: 'Sustainable Business Value',
                 technology: 'React Js, Visual Studio 2008',
                 description: 'SBV- Sustainable business value(web app using react with typescript) This tool supports you in computing the SBV(SBV is a collection of measures and assessments—not a single metric, and has a positive correlation with Total Shareholder Return)',
-                role: 'Developed responsive UI for web app using React Js using TypeScript for better quality and involved myself with unit testing of the UI code. Published a SonarQube report with good code coverage and minimal duplication of code and with zero code smells.',
+                role: 'Built a responsive UI for web app using React Js using TypeScript for better quality and involved myself with unit testing of the UI code. Published a SonarQube report with good code coverage and minimal duplication of code and with zero code smells.',
             },
             {
                 projectName: 'On Demand Knowledge',
@@ -51,7 +51,7 @@ const workExperience = [
         duration: 'May 2016 - Oct 2019',
         position: 'Senior System Engneer',
         location: "Bangalore",
-        description: 'During my tenure at Infosys Limited from May 2016 to October 2019, I held the position of UI Developer/ReactJs Developer. Here are the details of the project and my role:',
+        description: 'During my tenure at Infosys Limited from May 2016 to October 2019,  I made significant contributions in various aspects of project success. I started by gathering project requirements, ensuring alignment with the clients needs. Furthermore, I played a crucial role in creating visually appealing and user-friendly interfaces, seamlessly integrating complex UX designs. In response to project-specific needs, I took the initiative to enhance my React skills. My unwavering commitment to quality was evident as I utilized various technologies and frameworks to guarantee the development of top-notch user interfaces, improving data management, and enhancing the overall user experience.',
         logo: SiInfosys, // Replace with the actual logo file name
         projects: [
             {
@@ -60,6 +60,12 @@ const workExperience = [
                 description: 'CADRE consists of a web app for company managers to create, edit, and monitor trips for employees and candidates. It includes a dashboard to view all created trips. It also includes a mobile app to help employees and candidates monitor their trips and settle their expenses.',
                 role: 'Developed entire responsive UI for web app using React Js, which included complex UX design. Consumed web services through AJAX calls to display results in the required format and used React libraries to enhance the UI.',
             },
+            {
+                projectName: 'Agile Integration Broker',
+                technology: 'AngularJs, SQL',
+                description: 'project involved the development of an internal tool to manage deployment requests and defect tracking.',
+                role: 'Contributed to an internal tool for deployment requests and defect tracking using AngularJs and SQL Server, reducing manual deployment tracker time by more than 50%.',
+            }
         ],
     },
     // Add more work experience objects as needed
@@ -85,20 +91,22 @@ const WorkExperience = () => {
                                     <h4>{experience.position}</h4>
                                     <p>{experience.location}</p>
 
-                                    <p>{experience.description}</p>
+                                    <p className="exp-desc">{experience.description}</p>
+
+                                    <h4 className="work-project-nameProj">Projects :</h4>
                                     {experience.projects?.map((project, projectIndex) => (
-                                        <div key={projectIndex} className="work-project">
+                                        <ol key={projectIndex} className="work-project">
                                             <h4 className="work-project-name">{project.projectName}</h4>
-                                            <p className="work-project-technology">
-                                                <strong style={{ color: "grey" }}>Technology:</strong> {project.technology}
-                                            </p>
                                             <p className="work-project-description">
                                                 <strong style={{ color: "grey" }}>Description:</strong> {project.description}
                                             </p>
                                             <p className="work-project-role">
                                                 <strong style={{ color: "grey" }}>Role:</strong> {project.role}
                                             </p>
-                                        </div>
+                                            <p className="work-project-technology">
+                                                <strong style={{ color: "grey" }}>Technology:</strong> {project.technology}
+                                            </p>
+                                        </ol>
                                     ))}
                                 </div>
                             </div>
